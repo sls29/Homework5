8d8a23cb12a1821a375a4e6294e66994702dc2ae
roger <seles.lucian@gmail.com>
2023-03-17 15:11:02 +0200

    ver1

--- src/main/java/phone/App.java
+++ src/main/java/phone/App.java
@@ -8,6 +8,33 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+
+
+        Phone phone = new Galaxy6("Orange","0745121212",
+                "black", 100d, "1234QWERTY5678" );
+
+        phone.addContact("1", "phone number", "first name", "last name");
+
+        phone.addContact("2", "second phone number", "second first name", "second last name");
+
+//        phone.getFirstContact();
+
+//        phone.getLastContact();
+
+// send a message to the first contact from the previously listed
+
+// max number of characters - 100
+
+//        phone.sendMessage("phone number", "message content");
+
+//        phone.getFirstMessage("phone number");
+
+//        phone.getSecondMessage("phone number");
+
+// make a call to the second contact from the previously listed
+
+//        phone.call("second phone number");
+
+//        phone.viewHistory();
     }
 }
